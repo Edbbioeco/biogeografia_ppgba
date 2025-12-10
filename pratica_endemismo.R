f4655e45281cca8c069872c49ba173f37ffd3c37
Edson Silva-Júnior <edsonbbiologia@gmail.com>
2025-12-10 19:37:59 -0300

    Add files via upload

--- pratica_endemismo.R
+++ pratica_endemismo.R
@@ -8,6 +8,10 @@ library(terra)
 
 library(phyloraster)
 
+library(phangorn)
+
+library(ggtree)
+
 # Dados ----
 
 ## Importando ----
@@ -191,3 +195,61 @@ we_raster_ses <- phyloraster::rast.we.ses(occ_raster,
                                       occ_raster |> phyloraster::inv.range())
 
 we_raster_ses |> plot()
+
+# Análise de Parcimônia de Endemicidade ----
+
+## Construindo a Matriz -----
+
+matriz_pae <- grade |>
+  dplyr::left_join(grade_id |>
+                     sf::st_drop_geometry(),
+                   by = "id") |>
+  dplyr::mutate(presenca = 1,
+                id = id |> as.character()) |>
+  tidyr::drop_na() |>
+  sf::st_drop_geometry() |>
+  tidyr::pivot_wider(names_from = Espécie,
+                     values_from = presenca,
+                     values_fill = 0,
+                     values_fn = ~max(.))
+
+
+outgroup <- c("Outgroup", rep(0, 144)) |>
+  t() |>
+  as.data.frame() |>
+  dplyr::rename_all(~matriz_pae |> names()) |>
+  dplyr::mutate(dplyr::across(.cols = 2:145, ~as.numeric(.)))
+
+outgroup
+
+outgroup |> glimpse()
+
+matriz_pae <- matriz_pae |>
+  dplyr::bind_rows(outgroup) |>
+  tibble::column_to_rownames(var = "id") |>
+  as.matrix()
+
+matriz_pae
+
+## Criando um objeto phylodat ----
+
+pae_phydat <- matriz_pae |>
+  phangorn::phyDat(type = "USER",
+                   levels = c(0, 1))
+
+pae_phydat
+
+## Análise de parsimônia ----
+
+parsi <- pae_phydat |>
+  phangorn::pratchet()
+
+parsi |> class()
+
+## Gráfico ----
+
+parsi |>
+  ggtree::ggtree(layout = "circular",
+                 color = "black",
+                 linewidth = 1) +
+  ggtree::geom_tiplab(size = 5)
