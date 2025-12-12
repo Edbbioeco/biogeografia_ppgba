bbb7faac7f7dc34272104680a4c3af0840bb965a
Edson Silva-Júnior <edsonbbiologia@gmail.com>
2025-12-12 00:41:09 -0300

    Add files via upload

--- pratica_riqueza_diversidade.R
+++ pratica_riqueza_diversidade.R
@@ -14,16 +14,11 @@ library(writexl)
 
 library(terra)
 
-library(raster) #pacote usado para definir e trabalhar com celulas-grid
-library(sf) #pacote usado para visualizarmos os resulados
-library(rnaturalearthhires) #pacote usado para visualizarmos os resultados
-library(rnaturalearthdata) #pacote usado para visualizarmos os resultados
-library(viridis) #pacote usado para visualizarmos os resultados
-library(betapart) #pacote usado para fazermos diversidade-beta
-library(dplyr) #pacote usado para visualizarmos os resultados
-library(tidyr) #pacote usado para visualizarmos os resultados
-library(rnaturalearth) #pacote usado para visualizarmos os resultados
+library(tidyterra)
 
+library(betapart)
+
+library(sampbias)
 
 # Dados ----
 
@@ -144,6 +139,58 @@ gbif_coords_trat <- gbif_sf_trat |>
 
 gbif_coords_trat
 
+## Aeroportos ----
+
+### Importando ----
+
+aeroporto <- sf::st_read("DRIVERS/airports.shp")
+
+### Visualizando ----
+
+aeroporto
+
+ggplot() +
+  geom_sf(data = aeroporto)
+
+## Lagos ----
+
+### Importando ----
+
+lago <- sf::st_read("DRIVERS/lakes.shp")
+
+### Visualizando ----
+
+lago
+
+ggplot() +
+  geom_sf(data = lago)
+
+## Portos ----
+
+### Importando ----
+
+porto <- sf::st_read("DRIVERS/ports.shp")
+
+### Visualizando ----
+
+porto
+
+ggplot() +
+  geom_sf(data = porto)
+
+## Áreas urbanas ----
+
+### Importando ----
+
+urb <- sf::st_read("DRIVERS/urbanareas.shp")
+
+### Visualizando ----
+
+urb
+
+ggplot() +
+  geom_sf(data = urb)
+
 # Raster para a área do Brasil ----
 
 ## Gerando um grid ----
@@ -211,185 +258,128 @@ gbif_matriz <- gbif_sf_trat |>
   sf::st_join(br_grid) |>
   sf::st_drop_geometry() |>
   dplyr::mutate(Presenca = 1) |>
+  dplyr::distinct(species, id, .keep_all = TRUE) |>
   tidyr::pivot_wider(names_from = species,
                      values_from = Presenca,
-                     values_fill = 0,
-                     values_fn = ~max(.))
+                     values_fill = 0)
 
 gbif_matriz
 
+## Calcular Diversidade beta ----
 
+beta_calc <- gbif_matriz |>
+  tibble::column_to_rownames(var = "id") |>
+  dplyr::select(4:55) |>
+  betapart::betapart.core()
 
+beta_calc
 
+beta_pair <- gbif_matriz |>
+  tibble::column_to_rownames(var = "id") |>
+  dplyr::select(4:55) |>
+  beta.pair()
 
+beta_pair
 
+## Ajustando as matrizes para os componentes de diversidade beta ----
 
+beta_sor <- beta_pair$beta.sor |> as.matrix()
 
+beta_sim <- beta_pair$beta.sim |> as.matrix()
 
+beta_sne <- beta_pair$beta.sne |>  as.matrix()
 
+## Valores médios por célula ----
 
+beta_sor_mean <- beta_sor |> rowMeans(na.rm = TRUE)
 
-# Matriz comunidade: células × espécies (presença/ausência)
-mat_pa <- occ_id %>%
-  dplyr::select(id_cell, species) %>%
-  distinct() %>%
-  mutate(value = 1) %>%
-  pivot_wider(names_from = species, values_from = value, values_fill = 0) %>%
-  as.data.frame()
+beta_sim_mean <- beta_sim |> rowMeans(na.rm = TRUE)
 
-# Guardar os ID das células
-cell_ids <- mat_pa$id_cell
-mat_pa <- mat_pa[ , -1]
+beta_sne_mean <- beta_sne |> rowMeans(na.rm = TRUE)
 
+beta_df <- data.frame(id = gbif_matriz$id,
+                      beta_total = beta_sor_mean,
+                      turnover = beta_sim_mean,
+                      nestedness = beta_sne_mean)
 
-# Calcular os dados através do trabalho de Baselga (2010)
-beta_calc <- betapart.core(mat_pa)
+beta_df
 
-beta_pair <- beta.pair(beta_calc)
+## Rasterizando os três componentes ----
 
-# Matriz de faça a faça:
-beta_sor  <- as.matrix(beta_pair$beta.sor)  # beta total
-beta_sim  <- as.matrix(beta_pair$beta.sim)  # turnover
-beta_sne  <- as.matrix(beta_pair$beta.sne)  # nestedness
+rasters_beta <- function(componente){
 
-# Criando valores médios por célula (para mapear), os mapas de matrizes completas são difíceis de se ver, assim se usa o valor médio de beta entre cada célula e todas as demais
-beta_sor_mean <- rowMeans(beta_sor, na.rm = TRUE)
-beta_sim_mean <- rowMeans(beta_sim, na.rm = TRUE)
-beta_sne_mean <- rowMeans(beta_sne, na.rm = TRUE)
+  rast_beta <- br_grid |>
+    dplyr::left_join(beta_df,
+                     by = "id") |>
+    terra::vect() |>
+    terra::rasterize(vetor, field = componente)
 
-beta_df <- data.frame(
-  id_cell = cell_ids,
-  beta_total = beta_sor_mean,
-  turnover = beta_sim_mean,
-  nestedness = beta_sne_mean
-)
+  assign(paste0("raster_beta_", componente),
+         rast_beta,
+         envir = globalenv())
 
-#Convertendo o resultado para raster
-r_beta_total <- world_id |> plot()
-r_beta_total[] <- beta_df$beta_total[match(world_id[], beta_df$id_cell)]
-
-r_turnover <- world_id
-r_turnover[] <- beta_df$turnover[match(world_id[], beta_df$id_cell)]
-
-r_nestedness <- world_id
-r_nestedness[] <- beta_df$nestedness[match(world_id[], beta_df$id_cell)]
-
-# Plotando os resultados de diversidade seguindo mesmo padrão visto até aqui
-par(mfrow=c(1,3))
-
-plot(r_beta_total, col = viridis(100),
-     main = "Beta total (Sørensen)")
-plot(st_geometry(estados), add = TRUE, border = "black")
-
-plot(r_turnover, col = viridis(100),
-     main = "Turnover (Simpson)")
-plot(st_geometry(estados), add = TRUE, border = "black")
-
-plot(r_nestedness, col = viridis(100),
-     main = "Aninhamento (Nestedness)")
-plot(st_geometry(estados), add = TRUE, border = "black")
-
-#Salvando o mapa
-jpeg("beta_total_SpirostreptidaBR.jpeg", width = 2000, height = 1800, res = 300)
-plot(r_beta_total, col = viridis(100), main = "Beta total (Sørensen)")
-plot(st_geometry(estados), add = TRUE, border = "black")
-dev.off()
-
-jpeg("turnover_SpirostreptidaBR.jpeg", width = 2000, height = 1800, res = 300)
-plot(r_turnover, col = viridis(100), main = "Turnover (Simpson)")
-plot(st_geometry(estados), add = TRUE, border = "black")
-dev.off()
-
-jpeg("nestedness_SpirostreptidaBR.jpeg", width = 2000, height = 1800, res = 300)
-plot(r_nestedness, col = viridis(100), main = "Aninhamento (Nestedness)")
-plot(st_geometry(br_estados), add = TRUE, border = "black")
-dev.off()
-
-###################################################################################################
-
-# PASSO 7 - VERIFICANDO OS VIÉSES AMOSTRAIS E FATORES QUE INFLUENCIAM A DISTRIBUIÇÃO (EXCETO BIOCLIMÁTICOS)
-# aqui usaremos o pacote escrito por Zizka e colaboradores em 2017, publicado na Ecography
-
-#pacotes para abrir
-library(sampbias)
-
-#definindo novamente o diretório para evitar quaisquer erros
-setwd("C:/Users/Motion/Desktop/UFPE/Docência/2025.2/PPGBA/Biogeografia/Aulas/Aula 4 - DADOS/RESULTS")
-dir()
-
-#limpa-se todos os dados e chama o arquivo occ novamente
-occ <- read.table("gbif_records_clean.txt" , h = T, encoding = "UTF-8", sep= "\t", as.is = T)
-occ #conferindo
-
-#definindo a pasta onde estão nossos fatores a serem analisados
-setwd("C:/Users/Motion/Desktop/UFPE/Docência/2025.2/PPGBA/Biogeografia/Aulas/Aula 4 - DADOS/DRIVERS")
-dir()
-
-#carregar SHPs usando raster::shapefile() — retorna Spatial* diretamente
-air   <- terra::vect("airports.shp")
-lak      <- terra::vect("lakes.shp")
-por      <- terra::vect("ports.shp")
-urb  <- terra::vect("urbanareas.shp")
-
-
-#definindo os Gazetteers
-gazetteers <- list(
-  air = air,
-  lak = lak,
-  urb = urb,
-  por = por
-)
-
-#rodando a análise com resolução de 2.0 (~ 200km) e sem definir uma máscara aos dados
-bias.out <- calculate_bias(
-  x = occ,
-  gaz = gazetteers,
-  res = 2
-)
-
-#ver os dados gerados no output da análise
-summary(bias.out)
-
-# Gera o gráfico
-plot(bias.out)
-
-#salvando o output e os mapas no diretório dos resultados
-setwd("C:/Users/Motion/Desktop/UFPE/Docência/2025.2/PPGBA/Biogeografia/Aulas/Aula 4 - DADOS/RESULTS")
-capture.output(
-  summary(bias.out),
-  file = "bias_out_summary.txt"
-)
-
-jpeg("bias_out_plot.jpeg", width = 2000, height = 2000, res = 300)
-
-plot(bias.out)   # ou a função específica, se houver: plot(bias.out$bias_estimate) etc.
-
-dev.off()
-
-#projetar os resultados
-proj <- project_bias(bias.out)
-
-#gerar os mapas de viéses
-map_bias(proj, type = "log_sampling_rate")
-
-#apresenta no console todos os resulados obtidos como SpatRaster e os nomes [air,    air+por, air+por+lak, air+p~k+urb, Total~ntage, occurrences]
-proj
-sapply(proj, class)
-
-#criar rasters do mapa para cada variável, neste caso estamos usando em sequencia de resultados os vistos no comando anterior (prestar atenção nos nomes dos tiff e variável)
-for(i in 1:terra::nlyr(proj)) {
-  layer <- proj[[i]]
-  name  <- names(proj)[i]
-
-  terra::writeRaster(
-    layer,
-    filename = paste0("bias_", name, ".tif"),
-    overwrite = TRUE,
-    filetype = "GTiff",
-    gdal = c("COMPRESS=DEFLATE")
-  )
 }
 
+componentes <- beta_df[-1] |> names()
+
+componentes
+
+purrr::walk(componentes, rasters_beta)
+
+## Unindo os rasters ----
+
+beta_rasters <- ls(pattern = "raster_beta_") |>
+  mget(envir = globalenv()) |>
+  terra::rast()
+
+beta_rasters
+
+beta_rasters |> plot()
+
+## Visualizando ----
+
+ggplot() +
+  tidyterra::geom_spatraster(data = beta_rasters) +
+  geom_sf(data = estados, fill = NA) +
+  facet_wrap(~lyr) +
+  scale_fill_viridis_c()
+
+# Vieses de amosragem ----
+
+## Definindo os Gazetteers ----
+
+gazetteers <- list(air = aeroporto |> terra::vect(),
+                   lak = lago |> terra::vect(),
+                   urb = urb |> terra::vect(),
+                   por = porto |> terra::vect())
+
+gazetteers
+
+## Análise com resolução de 2.0 (~ 200km) e sem definir uma máscara aos dados ----
 
 
+bias.out <- sampbias::calculate_bias(x = gbif_coords_trat |>
+                                       dplyr::select(5:6),
+                                     gaz = gazetteers,
+                                     res = 2)
 
+bias.out
+
+bias.out |> dplyr::glimpse()
+
+bias.out |> plot()
+
+## Projetar os resultados ----
+
+proj <- bias.out |> sampbias::project_bias()
+
+proj
+
+ggplot() +
+  tidyterra::geom_spatraster(data = proj) +
+  scale_fill_viridis_c() +
+  facet_wrap(~lyr)
+
+## Mpas de viéses ----
+
+proj |> sampbias::map_bias(type = "log_sampling_rate")
